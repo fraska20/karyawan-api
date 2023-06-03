76ad27b5d7b99985109ee00e1532384efcdf51f8
Fraska <58796786+fraska20@users.noreply.github.com>
2023-06-03 14:54:57 +0700

    inisial

--- config/db.config.js
+++ config/db.config.js
@@ -5,10 +5,10 @@ const mysql = require('mysql');
 
 //local mysql db connection
 const dbConn = mysql.createConnection({
-  host     : process.env.HOST,
-  user     : process.env.USER,
-  password : process.env.PASSWORD,
-  database : process.env.DB
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_DATABASE
 });
 dbConn.connect(function(err) {
   if (err) throw err;
